47adb20b478505fb04832f24855dd20493336a11
pserrano778 <58224411+pserrano778@users.noreply.github.com>
2021-11-25 14:04:13 +0100

    fix in monsters list

--- botw_monsters-info_app/src/components/monsters/monstersList/monstersList.component.tsx
+++ botw_monsters-info_app/src/components/monsters/monstersList/monstersList.component.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import MonsterPreview from "../monsterPreview/monsterPreview.component";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import Nav from "react-bootstrap/Nav";
 // Monster interface
 interface Monster {
   id: number;
@@ -24,13 +23,11 @@ const MonstersList: React.FC<Props> = (props) => {
     return <p>There is no monster that matches the filter.</p>;
   }
   return (
-    <Nav>
-      <Container>
-        <Row xs="1" sm="2" md="3" lg="4" xl="5" xxl="6" className="justify-content-center">
-          {monsters.map((monster) => <MonsterPreview key={monster.id} name={monster.name} src={monster.image} />)}
-        </Row>
-      </Container>
-    </Nav>
+    <Container>
+      <Row xs="1" sm="2" md="3" lg="4" xl="5" xxl="6" className="justify-content-center">
+            {monsters.map((monster) => <MonsterPreview key={monster.id} name={monster.name} src={monster.image} />)}
+      </Row>
+    </Container>
   )
 }
 
